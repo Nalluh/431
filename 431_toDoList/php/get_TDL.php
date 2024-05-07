f5930a0a93a74e856f430f1bafc9c244772c9dca
Nalluh <120067040+Nalluh@users.noreply.github.com>
2024-05-06 17:35:50 -0700

    completed

--- 431_toDoList/php/get_TDL.php
+++ 431_toDoList/php/get_TDL.php
@@ -23,16 +23,50 @@ $list_checkBox = array();
 //firstname + lastname 
 $users_name = $user_data['FName'] ." ". $user_data[ 'LName']; 
 
-$query = "SELECT user_lists_items.text, user_lists.name, user_lists_items.isChecked FROM user_lists_items JOIN user_lists on user_lists_items.list_idx = user_lists.list_idx where user_idx = '{$user_data['idx']}' Order BY user_lists.created ASC, user_lists_items.isChecked DESC";
-$result = mysqli_query($con, $query);
+if(isset($_GET['selectedValue'])) {
+    $selectedValue = $_GET['selectedValue'];
+switch ($selectedValue) {
+    case 'sortName':
+        $query = "SELECT user_lists_items.text, user_lists.name, user_lists_items.isChecked FROM user_lists_items JOIN user_lists on user_lists_items.list_idx = user_lists.list_idx where user_idx = '{$user_data['idx']}' Order BY user_lists.name ASC, user_lists_items.isChecked DESC";
+        $result = mysqli_query($con, $query);
+        while($list_info = mysqli_fetch_assoc($result)){
+            array_push($list_name, $list_info['name']);
+            array_push($list_text,$list_info['text']);
+            array_push($list_checkBox,$list_info['isChecked']);
+        }
+        break;
+    case 'sortAsc':
+        $query = "SELECT user_lists_items.text, user_lists.name, user_lists_items.isChecked FROM user_lists_items JOIN user_lists on user_lists_items.list_idx = user_lists.list_idx where user_idx = '{$user_data['idx']}' Order BY user_lists.name ASC, user_lists_items.isChecked DESC";
+        $result = mysqli_query($con, $query);
+        while($list_info = mysqli_fetch_assoc($result)){
+            array_push($list_name, $list_info['name']);
+            array_push($list_text,$list_info['text']);
+            array_push($list_checkBox,$list_info['isChecked']);
+        }
+        break;
+    case 'sortDesc':
+        $query = "SELECT user_lists_items.text, user_lists.name, user_lists_items.isChecked FROM user_lists_items JOIN user_lists on user_lists_items.list_idx = user_lists.list_idx where user_idx = '{$user_data['idx']}' Order BY user_lists.name DESC, user_lists_items.isChecked DESC";
+        $result = mysqli_query($con, $query);
+        while($list_info = mysqli_fetch_assoc($result)){
+            array_push($list_name, $list_info['name']);
+            array_push($list_text,$list_info['text']);
+            array_push($list_checkBox,$list_info['isChecked']);
+        }
+        break;
+    default:
+    $query = "SELECT user_lists_items.text, user_lists.name, user_lists_items.isChecked FROM user_lists_items JOIN user_lists on user_lists_items.list_idx = user_lists.list_idx where user_idx = '{$user_data['idx']}' Order BY user_lists.created ASC, user_lists_items.isChecked DESC";
+    $result = mysqli_query($con, $query);
+    while($list_info = mysqli_fetch_assoc($result)){
+        array_push($list_name, $list_info['name']);
+        array_push($list_text,$list_info['text']);
+        array_push($list_checkBox,$list_info['isChecked']);
+    }
+}
+}
 
 // grab all list names corresponding to user idx and show them  in a dropdown menu (maybe)
 
-while($list_info = mysqli_fetch_assoc($result)){
-    array_push($list_name, $list_info['name']);
-    array_push($list_text,$list_info['text']);
-    array_push($list_checkBox,$list_info['isChecked']);
-}
+
 
 
 $user_info = array('email' => $user_data['email'], 'name' => $users_name, 'id' => $user_data['user_id'], 'list_name'=>$list_name, 'list_text' => $list_text, 'list_checkbox' => $list_checkBox);
